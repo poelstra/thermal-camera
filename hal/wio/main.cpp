0cb8bd89528da0af783dbbb9e825f2e04aff184a
Martin Poelstra <martin@beryllium.net>
2020-09-01 16:36:04 +0200

    hal/wio: Allow waiting for serial on bootup with joystick center pressed.

--- hal/wio/main.cpp
+++ hal/wio/main.cpp
@@ -1,5 +1,8 @@
 #include "app_main.h"
 
+#include "hal_led.h"
+#include "hal_time.h"
+
 #include "lcd.h"
 #include <Arduino.h>
 #include <lvgl.h>
@@ -7,6 +10,35 @@
 void setup()
 {
     Serial.begin(115200);
+
+    // Wait for serial monitor to connect when powering up with
+    // middle button pressed
+    pinMode(WIO_5S_PRESS, INPUT_PULLUP);
+    if (digitalRead(WIO_5S_PRESS) == 0)
+    {
+        while (!Serial)
+        {
+            hal_led(true);
+            hal_sleep(50);
+            hal_led(false);
+            hal_sleep(100);
+        }
+        Serial.println("Serial connected.");
+
+        // Wait until key released to prevent accidental other
+        // presses at startup
+        while (digitalRead(WIO_5S_PRESS) == 0)
+        {
+            hal_led(true);
+            hal_sleep(500);
+            hal_led(false);
+            hal_sleep(100);
+        }
+    }
+
+    Serial.println("Thermal Camera");
+    Serial.println("Copyright (C) 2020 Martin Poelstra");
+    Serial.println();
 
     lv_init();
     lcd_init();
