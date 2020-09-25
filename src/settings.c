51ff04fd83ff0f10c277976dab5044c41962b7f6
Martin Poelstra <martin@beryllium.net>
2020-09-25 23:14:10 +0200

    app: More intuitive reflected temperature settings.

--- src/settings.c
+++ src/settings.c
@@ -76,9 +76,9 @@ static void sync_settings(void)
     }
     else
     {
-        lv_obj_set_state(settings_win->reflected, LV_STATE_DEFAULT);
         if (lv_obj_get_group(settings_win->reflected) == NULL)
         {
+            lv_obj_set_state(settings_win->reflected, LV_STATE_DEFAULT);
             lv_group_t *group = lv_obj_get_group(settings_win->close);
             lv_group_remove_obj(settings_win->close);
             lv_group_add_obj(group, settings_win->reflected);
@@ -96,7 +96,7 @@ static void auto_ambient_event_cb(lv_obj_t *checkbox, lv_event_t event)
 {
     if (event == LV_EVENT_VALUE_CHANGED)
     {
-        settings_win->settings->auto_ambient = !lv_checkbox_is_checked(checkbox);
+        settings_win->settings->auto_ambient = lv_checkbox_is_checked(checkbox);
         sync_settings();
     }
 }
@@ -122,32 +122,45 @@ void settings_show(Settings *settings, SettingsClosedCallback closed_cb)
     settings_win->win = lv_win_create(lv_scr_act(), NULL);
     lv_win_set_title(settings_win->win, "Settings");
     lv_win_set_layout(settings_win->win, LV_LAYOUT_COLUMN_MID);
+    lv_win_set_scrollbar_mode(settings_win->win, LV_SCROLLBAR_MODE_HIDE);
 
+    // Emissivity
     lv_obj_t *row = lv_cont_create(settings_win->win, NULL);
     lv_coord_t padding = lv_obj_get_style_pad_inner(row, LV_CONT_PART_MAIN);
     lv_cont_set_fit2(row, LV_FIT_PARENT, LV_FIT_TIGHT);
     lv_cont_set_layout(row, LV_LAYOUT_ROW_MID);
-
     lv_obj_t *label = lv_label_create(row, NULL);
     lv_obj_t *spinbox = lv_spinbox_create(row, NULL);
     settings_win->emissivity = spinbox;
     lv_spinbox_set_range(spinbox, 0, 100);
     lv_spinbox_set_digit_format(spinbox, 3, 1);
     lv_spinbox_set_value(spinbox, settings_win->settings->emissivity * 100 + 0.5);
-
     lv_group_add_obj(group, spinbox);
     lv_label_set_long_mode(label, LV_LABEL_LONG_BREAK);
     lv_obj_set_width_margin(label, lv_obj_get_width_fit(row) - lv_obj_get_width_margin(spinbox) - padding);
     lv_label_set_text(label, "Emissivity");
 
+    // Reflected temperature
     row = lv_cont_create(settings_win->win, row);
     label = lv_label_create(row, label);
-    lv_obj_t *checkbox = lv_checkbox_create(row, NULL);
+
+    // Create a cell that fits both elements, but make sure that the container itself
+    // has enough space to allow e.g. outline to be drawn on elements when necessary, i.e.
+    // 'move' the row's padding into this container.
+    lv_obj_t *cell = lv_cont_create(row, NULL);
+    lv_cont_set_fit(cell, LV_FIT_TIGHT);
+    lv_obj_clean_style_list(cell, LV_CONT_PART_MAIN);
+    const lv_coord_t pad_outer = lv_obj_get_style_pad_right(row, LV_CONT_PART_MAIN);
+    lv_obj_set_style_local_margin_all(cell, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, -pad_outer);
+    lv_obj_set_style_local_pad_all(cell, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, pad_outer);
+    lv_obj_set_style_local_pad_inner(cell, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, padding);
+
+    lv_obj_t *checkbox = lv_checkbox_create(cell, NULL);
     settings_win->auto_ambient = checkbox;
     lv_group_add_obj(group, checkbox);
-    lv_checkbox_set_text(checkbox, "");
-    lv_checkbox_set_checked(checkbox, !settings_win->settings->auto_ambient);
-    spinbox = lv_spinbox_create(row, NULL);
+    lv_checkbox_set_text(checkbox, "Auto");
+    lv_checkbox_set_checked(checkbox, settings_win->settings->auto_ambient);
+    spinbox = lv_spinbox_create(cell, NULL);
     settings_win->reflected = spinbox;
     lv_spinbox_set_range(spinbox, -1000, +6000);
     lv_spinbox_set_digit_format(spinbox, 4, 3);
@@ -155,10 +168,11 @@ void settings_show(Settings *settings, SettingsClosedCallback closed_cb)
     lv_spinbox_step_prev(spinbox);
     lv_group_add_obj(group, spinbox);
     lv_label_set_long_mode(label, LV_LABEL_LONG_BREAK);
-    lv_obj_set_width_margin(label, lv_obj_get_width_fit(row) - lv_obj_get_width_margin(checkbox) -
-                                       lv_obj_get_width_margin(spinbox) - 2 * padding);
-    lv_label_set_text(label, "Custom reflected temperature");
+    lv_obj_set_width_margin(label, lv_obj_get_width_fit(row) - lv_obj_get_width_margin(cell) - padding - pad_outer);
+    lv_label_set_text(label, "Reflected temperature");
+    lv_cont_set_layout(cell, LV_LAYOUT_COLUMN_RIGHT);
 
+    // Close
     lv_obj_t *close_btn = lv_btn_create(settings_win->win, NULL);
     settings_win->close = close_btn;
     lv_obj_set_event_cb(close_btn, settings_close_cb);
@@ -166,6 +180,7 @@ void settings_show(Settings *settings, SettingsClosedCallback closed_cb)
     lv_obj_t *close_label = lv_label_create(close_btn, NULL);
     lv_label_set_text(close_label, "Close");
 
+    // Callbacks
     lv_obj_set_event_cb(settings_win->emissivity, emissivity_event_cb);
     lv_obj_set_event_cb(settings_win->reflected, reflected_event_cb);
     lv_obj_set_event_cb(settings_win->auto_ambient, auto_ambient_event_cb);
