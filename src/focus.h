0c63da593a9cd8c304499df9ef5e7c06f522f059
Martin Poelstra <martin@beryllium.net>
2020-10-15 21:09:48 +0200

    app: Docs.

--- src/focus.h
+++ src/focus.h
@@ -9,11 +9,15 @@ extern "C" {
 
 /**
  * Create new focus group and make it current.
+ *
+ * @returns the newly created group.
  */
 lv_group_t *focus_push_group();
 
 /**
  * Restore previous focus group.
+ *
+ * @returns the group that will now have focus.
  */
 lv_group_t *focus_pop_group();
 
