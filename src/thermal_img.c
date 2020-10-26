d1a073086f6c655ecfca326f69cd728252cae87c
Martin Poelstra <martin@beryllium.net>
2020-10-26 14:16:15 +0100

    app: Free up 50% of RAM by rendering thermal image on-demand.

--- src/thermal_img.c
+++ src/thermal_img.c
@@ -15,20 +15,9 @@
 #define LEGEND_WIDTH ((lv_coord_t)(16))
 #define LEGEND_HEIGHT ((lv_coord_t)(128))
 
-static lv_color_t thermal_pixels[THERMAL_IMG_WIDTH * THERMAL_IMG_HEIGHT];
-static lv_img_dsc_t thermal_img_desc = {
-    .header =
-        {
-            .cf = LV_IMG_CF_TRUE_COLOR,
-            .always_zero = 0,
-            .reserved = 0,
-            .w = THERMAL_IMG_WIDTH,
-            .h = THERMAL_IMG_HEIGHT,
-        },
-    .data_size = sizeof(thermal_pixels),
-    .data = (const uint8_t *)thermal_pixels,
-};
+static lv_design_cb_t thermal_img_ancestor_design;
 static lv_obj_t *thermal_img_obj;
+static uint8_t thermal_pixels[THERMAL_ROWS * THERMAL_COLS];
 
 static lv_color_t colorMap[256];
 
@@ -38,14 +27,87 @@ static lv_obj_t *measurement_min_label;
 static lv_obj_t *measurement_max_label;
 static lv_obj_t *measurement_center_label;
 
+static lv_design_res_t thermal_img_design(lv_obj_t *img, const lv_area_t *clip_area, lv_design_mode_t mode)
+{
+    if (mode == LV_DESIGN_COVER_CHK)
+    {
+        return thermal_img_ancestor_design(img, clip_area, mode);
+    }
+    else if (mode == LV_DESIGN_DRAW_MAIN)
+    {
+        lv_design_res_t res = thermal_img_ancestor_design(img, clip_area, mode);
+        if (res != LV_DESIGN_RES_OK)
+        {
+            return res;
+        }
+
+        lv_draw_rect_dsc_t draw_dsc;
+        lv_draw_rect_dsc_init(&draw_dsc);
+
+        lv_area_t bounds;
+        lv_area_copy(&bounds, clip_area);
+        bounds.x1 -= img->coords.x1;
+        bounds.x2 -= img->coords.x1;
+        bounds.y1 -= img->coords.y1;
+        bounds.y2 -= img->coords.y1;
+        bounds.x1 /= ZOOM;
+        bounds.y1 /= ZOOM;
+        bounds.x2 += ZOOM - 1;
+        bounds.y2 += ZOOM - 1;
+        bounds.x2 /= ZOOM;
+        bounds.y2 /= ZOOM;
+        if (bounds.x1 < 0)
+        {
+            bounds.x1 = 0;
+        }
+        if (bounds.y1 < 0)
+        {
+            bounds.y1 = 0;
+        }
+        if (bounds.x2 > THERMAL_COLS)
+        {
+            bounds.x2 = THERMAL_COLS;
+        }
+        if (bounds.y2 > THERMAL_ROWS)
+        {
+            bounds.y2 = THERMAL_ROWS;
+        }
+
+        for (uint16_t y = bounds.y1; y < bounds.y2; y++)
+        {
+            lv_coord_t y1 = y * ZOOM + img->coords.y1;
+            uint16_t y_index = y * THERMAL_COLS;
+            for (uint16_t x = bounds.x1; x < bounds.x2; x++)
+            {
+                lv_coord_t x1 = x * ZOOM + img->coords.x1;
+                lv_area_t coords;
+                lv_area_set(&coords, x1, y1, x1 + ZOOM - 1, y1 + ZOOM - 1);
+                draw_dsc.bg_color = colorMap[thermal_pixels[y_index + x]];
+                lv_draw_rect(&coords, clip_area, &draw_dsc);
+            }
+        }
+    }
+    else if (mode == LV_DESIGN_DRAW_POST)
+    {
+        return thermal_img_ancestor_design(img, clip_area, mode);
+    }
+
+    return LV_DESIGN_RES_OK;
+}
+
 void thermal_img_init()
 {
     colorscale_build(colorMap);
 
     // IR camera image
-    thermal_img_obj = lv_img_create(lv_scr_act(), NULL);
-    lv_img_set_src(thermal_img_obj, &thermal_img_desc);
+    thermal_img_obj = lv_obj_create(lv_scr_act(), NULL);
+    lv_obj_clean_style_list(thermal_img_obj, LV_OBJ_PART_MAIN);
+    lv_obj_set_size(thermal_img_obj, THERMAL_IMG_WIDTH, THERMAL_IMG_HEIGHT);
     lv_obj_align(thermal_img_obj, NULL, LV_ALIGN_IN_LEFT_MID, 4, 0);
+    thermal_img_ancestor_design = lv_obj_get_design_cb(thermal_img_obj);
+    lv_obj_set_design_cb(thermal_img_obj, thermal_img_design);
+    lv_obj_set_click(thermal_img_obj, false);
+    lv_obj_set_adv_hittest(thermal_img_obj, true);
 
     // Central pixel marker ('crosshair')
     const uint8_t crosshair_x = THERMAL_COLS / 2;
@@ -117,20 +179,7 @@ static float get_temp(const float *pixel, uint16_t x, uint16_t y)
     return pixel[x + y * THERMAL_COLS];
 }
 
-static void set_color(uint16_t x, uint16_t y, lv_color_t color)
-{
-    x *= ZOOM;
-    y *= ZOOM;
-    for (uint16_t dx = 0; dx < ZOOM; dx++)
-    {
-        for (uint16_t dy = 0; dy < ZOOM; dy++)
-        {
-            thermal_pixels[x + dx + (y + dy) * THERMAL_COLS * ZOOM] = color;
-        }
-    }
-}
-
-static lv_color_t temp_to_color(float temp)
+static uint8_t temp_to_intensity(float temp)
 {
     if (temp < thermal_min_temp)
     {
@@ -140,8 +189,12 @@ static lv_color_t temp_to_color(float temp)
     {
         temp = thermal_max_temp;
     }
-    uint8_t intensity = 255 * (temp - thermal_min_temp) / (thermal_max_temp - thermal_min_temp);
-    return colorMap[intensity];
+    return 255 * (temp - thermal_min_temp) / (thermal_max_temp - thermal_min_temp);
+}
+
+static lv_color_t temp_to_color(float temp)
+{
+    return colorMap[temp_to_intensity(temp)];
 }
 
 static uint32_t temp_to_rgb(float temp)
@@ -169,7 +222,7 @@ void thermal_img_update(const float pixels[THERMAL_ROWS * THERMAL_COLS])
             {
                 max_temp = pixel;
             }
-            set_color(x, y, temp_to_color(pixel));
+            thermal_pixels[y * THERMAL_COLS + x] = temp_to_intensity(pixel);
         }
     }
     lv_obj_invalidate(thermal_img_obj);
